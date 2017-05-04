d2106b3053a9e43ab17fbb974a89a20b066c7c17
Joseph Mienko <mienkoja@Josephs-MacBook-Pro.local>
2017-05-03 21:42:08 -0700

    ibid

--- R/load_measurement_objects.R
+++ R/load_measurement_objects.R
@@ -1,10 +1,15 @@
 load_measurement_objects <- function(){
+  
   object_names <- list_measurement_objects()
-  for(i in object_names){
-    filepath <- paste0(system.file('Data', package = 'oliveR')
-                       ,'/'
-                       ,i)
-    if(file.exists(filepath))
-      assign(i, readRDS(filepath), envir = .GlobalEnv)
+  
+  file_dir <- system.file("Data", package = "oliveR")
+  
+  if(!dir.exists(file_dir))
+    for(i in object_names){
+      filepath <- paste0(file_dir
+                         ,'/'
+                         ,i)
+      if(file.exists(filepath))
+        assign(i, readRDS(filepath), envir = .GlobalEnv)
   }  
 }
