01cf9cd3ac84dd2dd5f970efba0d80ac87be5997
Joseph Mienko <mienkoja@Josephs-MacBook-Pro.local>
2017-05-05 12:23:21 -0700

    fix error related to assignment of metric list

--- R/get_metric_json.R
+++ R/get_metric_json.R
@@ -1,19 +1,18 @@
-get_metric_json <- function(mpp_group = pcv_performance_monitoring, group_id){
+libraget_metric_json <- function(mpp_group = pcv_performance_monitoring, group_id){
+  
+  metric_list <- mpp_group$metric_list
+  
   measurement_names <- vector(mode = 'list', length = 0)
   measurements <- vector(mode = 'list', length = 0)
   providers <- vector(mode = 'list', length = 0)
   
   group <- list(group_id = group_id)
   
-  for (i in 1:length(mpp_list)){
+  for (i in 1:length(metric_list)){
     
-    mpp_group <- pcv_performance_monitoring
+    measurement_names[[i]] <- metric_list[[i]]$measurement_name
     
-    mpp_list <- mpp_group$metric_list
-    
-    measurement_names[[i]] <- mpp_list[[i]]$measurement_name
-    
-    measurements[[i]] <- list(value = mpp_list[[i]]$get_value(22)
+    measurements[[i]] <- list(value = metric_list[[i]]$get_value(22)
                               ,graph = NA) # will add graph in a later push 
     
     names(measurements) <- measurement_names
