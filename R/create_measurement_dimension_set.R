75e8aaddc52102677ea553d8827a94ba6f4f9ad7
Joe Mienko <mienkoja@gmail.com>
2017-05-15 13:20:49 -0700

    take care of formatting and rounding issues

--- R/create_measurement_dimension_set.R
+++ R/create_measurement_dimension_set.R
@@ -12,9 +12,22 @@ create_measurement_dimension_set <- function(mpp_group = NA
   metric_list <- mpp_group$metric_list
   metric_key <- metric_list[[characteristic_summary_obj]]$measurement_name
   
-  characteristic_summary_value = ifelse(!is.na(characteristic_summary_obj)
-                                        ,metric_list[[characteristic_summary_obj]]$get_value(group_id)
-                                        ,NA)
+  # get and format characteristic_summary_value
+  if (!all(pcv_performance_monitoring$metric_list[[characteristic_summary_obj]]$measurement_format == 'percent'
+          ,!is.na(characteristic_summary_obj))
+      ) {
+    characteristic_summary_value <- round(metric_list[[characteristic_summary_obj]]$get_value(group_id)
+                                          ,metric_list[[characteristic_summary_obj]]$measurement_rounding)
+  } else if (all(pcv_performance_monitoring$metric_list[[characteristic_summary_obj]]$measurement_format == 'percent'
+                  ,!is.na(characteristic_summary_obj))
+             ) {
+    characteristic_summary_value <- sprintf(paste0('%1.'
+                                                   ,metric_list[[characteristic_summary_obj]]$measurement_rounding
+                                                   ,'f%%')
+                                            ,100*metric_list[[characteristic_summary_obj]]$get_value(group_id))
+  }
+  
+  # get and format sub label (if supplied)
   characteristic_sub_label = ifelse(all(!is.na(characteristic_percent_conforming_obj) # valid percent conforming value
                                         ,!is.na(characteristic_summary_value) # valid summary value
                                         ,!is.na(sub_label_pre) # text in front of value
@@ -39,6 +52,7 @@ create_measurement_dimension_set <- function(mpp_group = NA
   characteristic_percent_conforming_graph = ifelse(!is.na(characteristic_percent_conforming_obj)
                                                      ,metric_list[[characteristic_percent_conforming_obj]]$get_donut(group_id)
                                                      ,NA)
+  
   characteristic_data_quality_value = ifelse(!is.na(characteristic_data_quality_obj)
                                              ,metric_list[[characteristic_data_quality_obj]]$get_value(group_id)
                                              ,NA)
