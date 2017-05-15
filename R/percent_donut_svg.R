e3fe364970a39d8c064fa9a090b25054cc16b25e
Joe Mienko <mienkoja@gmail.com>
2017-05-15 15:01:29 -0700

    fix another problem with donuts (make sure order is preserved from df to geom_rect)

--- R/percent_donut_svg.R
+++ R/percent_donut_svg.R
@@ -1,18 +1,32 @@
 percent_donut_svg = function(proportion = .4
-                                ,fill = c("#FC4A1A", "#dfdce3")
+                                ,fill = c("#FC4A1A", "#DFDCE3")
                                 ,thickness_out = 4
-                                ,thickness_in = 3
-                                ,inner_radius = 50
-                                ,outer_radius = 100){
+                                ,thickness_in = 3){
   
   # Create test data.
+  # proportion <- .4
+  # dat1 <- data.frame(prop=c(proportion, 1-proportion))
+  # 
+  # 
+  # # Add addition columns, needed for drawing with geom_rect.
+  # dat1$fraction <- dat1$prop / sum(dat1$prop)
+  # #dat <- dat[order(dat$fraction), ]
+  # dat1$ymax <- cumsum(dat1$fraction)
+  # dat1$ymin <- c(0, head(dat1$ymax, n=-1))
+  # dat1$fill <- factor(fill, levels = fill[order(fill)])
+  # 
+  # proportion <- .6
+  
   dat <- data.frame(prop=c(proportion, 1-proportion))
+  
   
   # Add addition columns, needed for drawing with geom_rect.
   dat$fraction <- dat$prop / sum(dat$prop)
-  dat <- dat[order(dat$fraction), ]
+  #dat <- dat[order(dat$fraction), ]
   dat$ymax <- cumsum(dat$fraction)
   dat$ymin <- c(0, head(dat$ymax, n=-1))
+  dat$fill <- factor(fill, levels = fill[order(fill, decreasing = TRUE)])
+  
   
   plot <- dat %>%     
     ggplot(aes(fill=fill
