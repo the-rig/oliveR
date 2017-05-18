37447a390ae24bfd52ca330f36abf3cf8bd456fa
Joe Mienko <mienkoja@gmail.com>
2017-05-18 13:24:26 -0700

    second round of jitter changes, this time modifying define_var_attributes to jitter

--- R/define_var_attribute.R
+++ R/define_var_attribute.R
@@ -3,8 +3,15 @@
 
 define_var_attribute <- function(data
                                ,population_member_id
-                               ,value){
-  
+                               ,value
+                               ,jitter = Sys.getenv("OLIVER_REPLICA_JITTER")){
+
+  data = tbl_person_child_record_count
+  population_member_id = 'id_referral_visit'
+  value = 'child_count_attr'
+
+  mean_value <- lapply(data[,value], mean, na.rm = TRUE)
+
   dots <- setNames(list(lazyeval::interp(~ x
                                ,x = as.name(value)))
                    ,value)
@@ -15,5 +22,21 @@ define_var_attribute <- function(data
     mutate_(.
             ,.dots = dots) %>%
     rename_(., .dots = setNames(value, "attr_values"))
+
+  if (lapply(data[,value], class) == 'logical') {
+
+    attribute <- attribute %>%
+      mutate(attr_values = if(jitter){runif(n())} else attr_values
+             ,attr_values = ifelse(attr_values > mean_value, TRUE, FALSE))
+
+  } else if (any(lapply(data[,value], class) == 'integer'
+                     ,lapply(data[,value], class) == 'double')) {
+    attribute <- attribute %>%
+      mutate(attr_values = if(jitter){attr_values + rbinom(n = n()
+                                                           ,size = round(as.numeric(mean_value), digits = 0)
+                                                           ,prob = runif(1))} else attr_values)
+  }
+
   return(attribute)
+
 }
