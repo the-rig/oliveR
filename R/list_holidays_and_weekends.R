2d86d3411768797803cbfeb02951adb11bdd2e10
Joe Mienko <mienkoja@gmail.com>
2017-05-22 17:41:42 -0700

    fix some documentation warnings that have been causes (apparently) by unescaped percent symbols

--- R/list_holidays_and_weekends.R
+++ R/list_holidays_and_weekends.R
@@ -1,9 +1,9 @@
 #' Create a vector of 24 hour Intervals \pkg{lubridate} from \code{oliver_replica} write a single file
 #'
-#' This function is used to generate a vector of holiday and weekend \code{Interval} objects as defined by \pkg{lubridate}. It requires an active internet connection as it makes use of the Enrico service available at \link{http://www.kayaposoft.com/enrico/} to determine holidays for a given locality. This is used to exclude weekends and holidays from \code{Duration} and \code{Period} calculations. 
+#' This function is used to generate a vector of holiday and weekend \code{Interval} objects as defined by \pkg{lubridate}. It requires an active internet connection as it makes use of the Enrico service available at \link{http://www.kayaposoft.com/enrico/} to determine holidays for a given locality. This is used to exclude weekends and holidays from \code{Duration} and \code{Period} calculations.
 #'
-#' @param from_date A character string specifying the start date of the \code{Interval} vector in the format "%m-%d-%Y". Defaults to '01-01-2011'
-#' @param to_date A character string specifying the stop date of the \code{Interval} vector in the format "%m-%d-%Y". Defaults to '01-01-2111'.
+#' @param from_date A character string specifying the start date of the \code{Interval} vector in the format \code{\%m-\%d-\%Y}. Defaults to \code{01-01-2011}
+#' @param to_date A character string specifying the stop date of the \code{Interval} vector in the format \code{\%m-\%d-\%Y}. Defaults to \code{01-01-2111}.
 #' @param country_filter A character string specifying country of the holidays as required by the Enrico service. Defaults to 'usa'.
 #' @param region_filter A character string specifying region, of the country, of the holidays as required by the Enrico service. Defaults to 'Washington'.
 #'
@@ -14,37 +14,37 @@
 #' @examples
 #'
 #' list_holidays_and_weekends()
-#' 
+#'
 
 
 list_holidays_and_weekends <- function(from_date = '01-01-2011'
                                        ,to_date = '01-01-2111'
                                        ,country_filter = 'usa'
                                        ,region_filter = 'Washington'){
-  content_url <- paste0("http://www.kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForDateRange&=", 
-                        "&fromDate=", from_date, "&toDate=", to_date, "&country=", 
+  content_url <- paste0("http://www.kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForDateRange&=",
+                        "&fromDate=", from_date, "&toDate=", to_date, "&country=",
                         country_filter, "&region=", region_filter)
   GET_content <- GET(content_url)
-  holidays <- content(GET_content, as = "text", encoding = 'UTF-8') %>% as.tbl_json() %>% 
-    gather_array() %>% enter_object("date") %>% spread_values(day = jstring("day"), 
-                                                              month = jstring("month"), year = jstring("year")) %>% 
-    mutate(holidate = paste0(year, str_pad(month, 2, pad = "0"), 
-                             str_pad(day, 2, pad = "0")), holidate = ymd(holidate)) %>% 
+  holidays <- content(GET_content, as = "text", encoding = 'UTF-8') %>% as.tbl_json() %>%
+    gather_array() %>% enter_object("date") %>% spread_values(day = jstring("day"),
+                                                              month = jstring("month"), year = jstring("year")) %>%
+    mutate(holidate = paste0(year, str_pad(month, 2, pad = "0"),
+                             str_pad(day, 2, pad = "0")), holidate = ymd(holidate)) %>%
     .$holidate
-  day_seq <- seq(from = dmy(from_date), to = dmy(to_date), 
+  day_seq <- seq(from = dmy(from_date), to = dmy(to_date),
                  by = "days")
-  weekend_days <- day_seq[wday(day_seq, label = TRUE) %in% 
+  weekend_days <- day_seq[wday(day_seq, label = TRUE) %in%
                             c("Sat", "Sun")]
-  holiday_intervals <- interval(ymd_hms(paste0(holidays, " 00:00:00"), 
-                                        tz = "America/Los_Angeles"), ymd_hms(paste0(holidays, 
+  holiday_intervals <- interval(ymd_hms(paste0(holidays, " 00:00:00"),
+                                        tz = "America/Los_Angeles"), ymd_hms(paste0(holidays,
                                                                                     " 00:00:00"), tz = "America/Los_Angeles") + hours(24))
   weekend_intervals <- interval(
     ymd_hms(paste0(weekend_days, " 00:00:00"), tz = "America/Los_Angeles")
     ,ymd_hms(paste0(weekend_days, " 00:00:00"), tz = "America/Los_Angeles") + hours(24))
-  
+
   interval_df <- rbind(data.frame(interval = holiday_intervals), data.frame(interval = weekend_intervals))
-  
+
   day_exclusions <- interval_df$interval
-  
+
   return(day_exclusions)
 }
