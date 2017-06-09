19d10ec689a3e7bf6859777b4ddb7d475c881dfc
Joe Mienko <mienkoja@gmail.com>
2017-06-08 18:31:45 -0700

    allow user to specify a boolean option for setting converting to a simple ymd (from ymd hms)

--- R/define_var_event.R
+++ R/define_var_event.R
@@ -5,6 +5,7 @@
 define_var_event <- function(data
                              ,id
                              ,value_date
+                             ,convert_to_ymd = TRUE
                              ,tz = 'America/Los_Angeles'){
   dots <- setNames(list(lazyeval::interp(~ as.POSIXct(x, tz = tz)
                                ,x = as.name(value_date)))
@@ -14,6 +15,7 @@ define_var_event <- function(data
                    ,value_date) %>%
     as_data_frame() %>%
     mutate_(.
-            ,.dots = dots)
+            ,.dots = dots) %>%
+    mutate(value_date = ifelse(convert_to_ymd, as_date(value_date), value_date))
   return(event)
 }
