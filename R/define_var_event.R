5a9ffce83129abd964b77347ffea57005f298155
Joe Mienko <mienkoja@gmail.com>
2017-06-09 13:33:38 -0700

    replace vectorized if-then logic

--- R/define_var_event.R
+++ R/define_var_event.R
@@ -7,15 +7,32 @@ define_var_event <- function(data
                              ,value_date
                              ,convert_to_ymd = TRUE
                              ,tz = 'America/Los_Angeles'){
-  dots <- setNames(list(lazyeval::interp(~ as.POSIXct(x, tz = tz)
+  dots1 <- setNames(list(lazyeval::interp(~ as.POSIXct(x, tz = tz)
                                ,x = as.name(value_date)))
                    ,value_date)
-  event <- select_(data
-                   ,id
-                   ,value_date) %>%
-    as_data_frame() %>%
-    mutate_(.
-            ,.dots = dots) %>%
-    mutate(value_date = ifelse(convert_to_ymd, as_date(value_date), value_date))
+  dots2 <- setNames(list(lazyeval::interp(~ as_date(x, tz = tz)
+                                         ,x = as.name(value_date)))
+                   ,value_date)
+
+
+  if(convert_to_ymd) {
+    event <- select_(data
+                     ,id
+                     ,value_date) %>%
+      as_data_frame() %>%
+      mutate_(.
+              ,.dots = dots1) %>%
+      mutate_(.
+              ,.dots = dots2)
+  } else {
+    event <- select_(data
+                     ,id
+                     ,value_date) %>%
+      as_data_frame() %>%
+      mutate_(.
+              ,.dots = dots1)
+  }
+
+
   return(event)
 }
