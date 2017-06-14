59493c575531ea1bbba2510cc7bc5cb43a24fe2d
Joe Mienko <mienkoja@gmail.com>
2017-06-14 13:57:23 -0700

    adding concat conditions back into the dimension set

--- R/create_measurement_dimension_set.R
+++ R/create_measurement_dimension_set.R
@@ -125,10 +125,10 @@ create_measurement_dimension_set <- function(mpp_group = NA
   }
 
 
-  # if(any(!is.na(sub_label_pre), !is.na(sub_label_post))){
-  #   sub_value <- paste0(sub_label_pre, sub_value, sub_label_post)
-  # } else
-  #   sub_value <- 'NA'
+  if(any(!is.na(sub_label_pre), !is.na(sub_label_post))){
+    sub_value <- paste0(sub_label_pre, sub_value, sub_label_post)
+  } else
+    sub_value <- 'NA'
   #
   # if (any(stringr::str_detect(value, 'NA')
   #         ,stringr::str_detect(value, 'NaN'))) {
