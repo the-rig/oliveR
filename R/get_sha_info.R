bfbc2c690012d934bd12267780cb7e2e3f96b5c7
Joe Mienko <mienkoja@gmail.com>
2017-07-12 07:29:03 -0700

    fix the overzealous escaping in my regex

--- R/get_sha_info.R
+++ R/get_sha_info.R
@@ -1,9 +1,9 @@
 get_sha_info <- function(){
-  sha_file <- tryCatch(paste0(readLines("R/install-packages.R"), collapse = "")
+  sha_file <- tryCatch(paste0(readLines("/app/R/install-packages.R"), collapse = "")
                        ,warning=function(w) 1)
   if(sha_file==1) {
     sha_value <- 'SHA information not available'
   } else
-    sha_value <- stringr::str_extract(sha_file, stringr::regex('(?<=oliveR@).*(?=\\\\")', multiline = TRUE))
+    sha_value <- stringr::str_extract(sha_file, stringr::regex('(?<=oliveR@).*(?=\\")', multiline = TRUE))
   return(sha_value)
 }
